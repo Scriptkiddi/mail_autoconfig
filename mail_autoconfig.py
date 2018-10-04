e1703a45ed27e434703c34f45fd51965a5c61b3c
ajs124 <git@ajs124.de>
2018-10-04 21:56:04 +0200

    add gevent wsgi serve. oops

--- mail_autoconfig.py
+++ mail_autoconfig.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 from flask import Flask, render_template, request, Response
 app = Flask(__name__)
 
@@ -7,3 +8,8 @@ def tb_autoconf():
     domain = request.host.replace('autoconfig.', '')
     xml = render_template('template.xml', domain=domain)
     return Response(xml, mimetype='text/xml')
+
+if __name__ == "__main__":
+    from gevent.pywsgi import WSGIServer
+    http_server = WSGIServer(('127.0.0.1', int(os.environ['LISTEN_PORT'])), app)
+    http_server.serve_forever()
